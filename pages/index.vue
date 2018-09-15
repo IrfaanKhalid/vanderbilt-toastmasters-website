e3cf3aa32880c549c3a5b266ae77711598cad243
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-09-15 12:39:16 -0500

    moved style stuff from index to mission

--- pages/index.vue
+++ pages/index.vue
@@ -46,26 +46,6 @@ body {
   align-items: center;
 }
 
-.mission {
-  min-height: 100vh;
-  min-width: 100vw;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-areas: "statement img" "description img";
-  justify-content: center;
-  align-items: center;
-  background-color: #d8ab4c;
-}
-
-.mission-statement {
-  grid-area: statement;
-}
-
-.mission-description {
-  grid-area: description;
-}
-
 .title {
   font-family: Roboto, "Quicksand", "Source Sans Pro", -apple-system,
     BlinkMacSystemFont, "Segoe UI", "Helvetica Neue", Arial, sans-serif;
